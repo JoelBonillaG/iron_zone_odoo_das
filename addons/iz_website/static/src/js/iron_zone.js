c024d6b3b76dcc9ce0edeb8bc1fb0a477d45fb4b
David Manjarres <davidmanjarres2004@gmail.com>
2026-05-05 02:04:40 -0500

    styles(website): edit

--- addons/iz_website/static/src/js/iron_zone.js
+++ addons/iz_website/static/src/js/iron_zone.js
@@ -2,24 +2,26 @@
 
 import { publicWidget } from "@web/legacy/js/public/public_widget";
 
-publicWidget.registry.IronZoneQuantity = publicWidget.Widget.extend({
-    selector: '.js_add_cart_json',
-    events: {
-        'mousedown': '_onMouseDown',
-        'mouseup': '_onMouseUp',
-        'mouseleave': '_onMouseUp',
-    },
+if (publicWidget && publicWidget.registry) {
+    publicWidget.registry.IronZoneQuantity = publicWidget.Widget.extend({
+        selector: ".js_add_cart_json",
+        events: {
+            mousedown: "_onMouseDown",
+            mouseup: "_onMouseUp",
+            mouseleave: "_onMouseUp",
+        },
 
-    _onMouseDown: function (ev) {
-        const btn = ev.currentTarget;
-        if (btn.querySelector('.fa-plus') || btn.querySelector('.fa-minus')) {
-            this.interval = setInterval(() => btn.click(), 150);
-        }
-    },
+        _onMouseDown: function (ev) {
+            const btn = ev.currentTarget;
+            if (btn.querySelector(".fa-plus") || btn.querySelector(".fa-minus")) {
+                this.interval = setInterval(() => btn.click(), 150);
+            }
+        },
 
-    _onMouseUp: function () {
-        if (this.interval) {
-            clearInterval(this.interval);
-        }
-    },
-});
+        _onMouseUp: function () {
+            if (this.interval) {
+                clearInterval(this.interval);
+            }
+        },
+    });
+}
