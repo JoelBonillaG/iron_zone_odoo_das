8be2fa3a3aa1376f94957c5bd14f6ee1020f74f4
carol104 <carol.28427@gmail.com>
2026-05-17 16:53:11 -0500

    feat: The available seats and the price were added.

--- seeds/07_event_classes.py
+++ seeds/07_event_classes.py
@@ -384,12 +384,80 @@ def ensure_event_login_required(uid, models):
         },
         fields=["id"]
     )
+def ensure_event_ticket_info(uid, models):
+    inherit_id = models.execute_kw(DB, uid, PASSWORD, 'ir.ui.view', 'search', [[('key', '=', 'website_event.event_description_full')]])
+    if not inherit_id:
+        return
+    
+    arch = '''
+    <data>
+        <xpath expr="//aside[@id='o_wevent_event_main_sidebar']/div[hasclass('d-none', 'd-lg-block', 'border-bottom', 'pb-2', 'mb-3')]" position="before">
+            <div class="o_wevent_sidebar_block border-bottom pb-3 mb-4 d-none d-lg-block">
+                <h6 class="o_wevent_sidebar_title">Información del Boleto</h6>
+                <div class="mb-2 d-flex align-items-center">
+                    <i class="fa fa-ticket fa-fw me-2 text-muted"/> 
+                    <strong class="me-2">Precio:</strong> 
+                    <t t-if="event.event_ticket_ids and event.event_ticket_ids[0].price > 0">
+                        <span t-out="event.event_ticket_ids[0].price" t-options="{'widget': 'monetary', 'display_currency': website.currency_id}"/>
+                    </t>
+                    <t t-else="">
+                        <span class="badge text-bg-success">Gratis</span>
+                    </t>
+                </div>
+                <div class="mb-2 d-flex align-items-center">
+                    <i class="fa fa-users fa-fw me-2 text-muted"/> 
+                    <strong class="me-2">Cupos Disponibles:</strong> 
+                    <span t-out="event.seats_available"/> / <span t-out="event.seats_max"/>
+                </div>
+            </div>
+        </xpath>
+        
+        <xpath expr="//header[hasclass('d-lg-none', 'mt-4', 'mb-2', 'py-3', 'border-top')]" position="after">
+            <div class="o_wevent_sidebar_block border-bottom pb-3 mb-4 d-lg-none mt-3">
+                <h6 class="o_wevent_sidebar_title">Información del Boleto</h6>
+                <div class="mb-2 d-flex align-items-center">
+                    <i class="fa fa-ticket fa-fw me-2 text-muted"/> 
+                    <strong class="me-2">Precio:</strong> 
+                    <t t-if="event.event_ticket_ids and event.event_ticket_ids[0].price > 0">
+                        <span t-out="event.event_ticket_ids[0].price" t-options="{'widget': 'monetary', 'display_currency': website.currency_id}"/>
+                    </t>
+                    <t t-else="">
+                        <span class="badge text-bg-success">Gratis</span>
+                    </t>
+                </div>
+                <div class="mb-2 d-flex align-items-center">
+                    <i class="fa fa-users fa-fw me-2 text-muted"/> 
+                    <strong class="me-2">Cupos Disponibles:</strong> 
+                    <span t-out="event.seats_available"/> / <span t-out="event.seats_max"/>
+                </div>
+            </div>
+        </xpath>
+    </data>
+    '''
+    
+    create_or_update(
+        uid,
+        models,
+        "ir.ui.view",
+        [("key", "=", "iz_event_ticket_info")],
+        {
+            "name": "Show Event Ticket Info",
+            "key": "iz_event_ticket_info",
+            "type": "qweb",
+            "mode": "extension",
+            "inherit_id": inherit_id[0],
+            "arch": arch,
+            "active": True
+        },
+        fields=["id"]
+    )
 
 
 def run():
     uid, models = connect()
     ensure_event_admin_rule(uid, models)
     ensure_event_login_required(uid, models)
+    ensure_event_ticket_info(uid, models)
     archive_old_demo_events(uid, models)
 
     # Get company info
