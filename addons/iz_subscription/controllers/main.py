4e16105490953ed0fe1da5685aa14a5e6394f10b
carol104 <carol.28427@gmail.com>
2026-06-02 22:31:32 -0500

    fix: arrange some errors

--- addons/iz_subscription/controllers/main.py
+++ addons/iz_subscription/controllers/main.py
@@ -11,7 +11,7 @@ class IzWebsiteEventSaleController(WebsiteEventSaleController):
         if user and not user._is_public():
             partner = user.partner_id
             benefit = partner._get_current_subscription_benefits("events")[:1]
-            if benefit and benefit.benefit_type == "free":
+            if benefit and (benefit.benefit_type == "free" or benefit.discount_percent == 100):
                 # User has a free events benefit. Bypass the cart creation!
                 return WebsiteEventController.registration_confirm(self, event, **post)
         
